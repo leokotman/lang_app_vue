3c3209f76fbf36f63d6ac754bdaadcdee9518c43
Leo <leoktmn@yandex.ru>
2022-07-28 16:07:18 +0200

    changed repo name

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:leokotman/weborigo_lang_vue.git main:gh-pages
+git push -f git@github.com:leokotman/lang_app_vue.git main:gh-pages
 
 cd -
